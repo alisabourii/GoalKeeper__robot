edc3ff2d7bc96c8acd40ed354b8ce41a20f02216
alisabourii <72344723+alisabourii@users.noreply.github.com>
2022-11-07 22:48:37 +0300

    Add files via upload

---
+++ Akili_Kale.ino
@@ -0,0 +1,71 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd_1(12, 11, 5, 4, 3, 2);
+int trig = 6;
+int echo = 7;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10, INPUT);
+  lcd_1.begin(16, 2);
+  lcd_1.setCursor(2,0);
+  lcd_1.print("HAK");
+
+  lcd_1.setCursor(10,0);
+  lcd_1.print("PUAN");
+
+
+}
+
+int hak=0, puan=0;
+
+void loop() {
+
+  while (hak < 3)
+  {
+   if (digitalRead(10) == 1){
+      hak += 1;
+      digitalWrite(9,1);
+      delay(200);
+      digitalWrite(9,0);
+      delay(300);}
+   //------Mesafeye Hesaplama ve PUAN eklemk-------------
+   digitalWrite(trig,1);//Trig
+   delay(1);
+   digitalWrite(trig,0);
+   int sure = pulseIn(echo,1);
+   int mesafe = (sure/2)/28.97;
+   int msf = round(mesafe);
+
+   if(msf <= 20)
+    {  puan += 30;
+      digitalWrite();
+      hak += 1;
+       delay(1000);
+    }
+   else if(msf <= 30)
+    {  puan += 15;
+       hak += 1;
+       delay(1000);
+    }
+  //-----------------------------------
+
+   lcd_1.setCursor(3,1);
+   lcd_1.print(hak);
+   lcd_1.setCursor(11,1);
+   lcd_1.print(puan);
+  }
+  lcd_1.clear();
+  lcd_1.setCursor(5,0);
+  lcd_1.print("SONUC");
+  lcd_1.setCursor(7,1);
+  lcd_1.print(puan);
+  
+
+
+delay(1000);
+}
